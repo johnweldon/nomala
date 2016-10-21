8d4ae57b8560feb95dc5edb7122ef5376129e342
John Weldon <johnweldon4@gmail.com>
2016-10-21 11:51:15 -0700

    json parsing and extracting

--- build.sbt
+++ build.sbt
@@ -5,5 +5,8 @@ version := "0.1"
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
-  "org.apache.httpcomponents" % "httpclient" % "4.5.2"
+  "org.apache.httpcomponents" % "httpclient" % "4.5.2",
+  "net.liftweb" %% "lift-json" % "2.+"
 )
+
+
--- src/main/scala/Nomala.scala
+++ src/main/scala/Nomala.scala
@@ -1,7 +1,24 @@
+import net.liftweb.json._
+
 object Nomala {
+  implicit val formats = DefaultFormats
+
   def main(args: Array[String]) = {
-    println("Hi!")
-    println()
-    println(WebClient.getResource("http://localhost:4646/v1/nodes"))
+    val js = WebClient.getResource("http://localhost:4646/v1/nodes")
+    val json = parse(js)
+    for (node <- json.children) {
+      val m = node.extract[Node]
+      println(m.Name)
+      println(m.Datacenter)
+      println(m.ID)
+    }
   }
 }
+
+case class Node
+(
+  ID: String,
+  Name: String,
+  Datacenter: String
+)
+
